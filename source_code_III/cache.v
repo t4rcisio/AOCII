d9bb6a1b58960cd97f18f04ff1c5fc13ad5fad24
t4rcisio <tarcisiobatistaprates@gmail.com>
2023-04-05 00:06:42 -0300

    ram duas vias

--- source_code_III/cache.v
+++ source_code_III/cache.v
@@ -1,73 +1,145 @@
 
 
 
-
-
 module cache(clock, index, tag, data, mode, data_out);
 
 input clock;
 input [1:0] index; // Acesso à posição da cache
 input [7:0] tag;   // TAG da instrução
 input [7:0] data;  // Dado a ser gravado
-input mode; 		    // Leitura ou escrita
+input mode; 		 // Leitura ou escrita
 
 output [7:0] data_out;
 
 
+reg [4:0]valid;
+reg [4:0]dirt;
+reg [4:0]lru;
+
+
 reg hit;
 reg miss;
+reg w_back;
 
-reg [19:0] memCache [7:0];
+reg [18:0] memCache [0:7];
+
+reg [7:0] RAM [0:255];
+
 reg [2:0] index_plus;
 
-reg [7:0] temp;
+initial begin
+
+valid = 18;
+dirt = 17;
+lru = 16;
+
+memCache[0] = 0;
+memCache[1] = 0;
+memCache[2] = 0;
+memCache[3] = 0;
+memCache[4] = 0;
+memCache[5] = 0;
+memCache[6] = 0;
+memCache[7] = 0;
+
+end
+
 
 
 //	  Valid	Dirt	  LRU	   TAG	 Data
 // 	 1 	  1      1		 8		  8
-//	  [19]   [18]   [16]  [15:8]  [7:0]
+//	  [18]   [17]   [16]  [15:8]  [7:0]
 
 
 always @(negedge clock) begin
 		
-		index_plus = {1'b0,index} + 3'b100;
-
+		
+		index_plus = {1'b0, index} + 3'b100;
+	
 		if (mode) begin // Gravacao
 			
 			// CANAL 1
-			if (memCache[index][15:8] == tag) begin
-					hit <= 1;
+			// GRAVACAO VAZIA
+			if (!memCache[index][valid] || (!memCache[index][dirt] && !memCache[index][lru])) begin
 					
-			end else begin
-					miss <= 1;
-			
+					memCache[index][valid] = 1; 		// Marca a validade
+					memCache[index][dirt] = 1;			// Marca que está sujo
+					
+					memCache[index][lru] = 1;			// Altera o LRU
+					memCache[index_plus][lru] = 0;	// LRU da segunda via
+					
+					memCache[index][15:8] = tag;		// Atualiza a TAG
+					memCache[index][7:0] = data;		// Atualiza o dado
+					
+					miss = 1;
+					hit = 0;
+					w_back = 0; 
 			end
 			
-			// CANAL 2
-			if (memCache[index_plus][15:8] == tag) begin
-					hit <= 1;
+			// GRAVACAO SUJA -> Valid e Dirt
+			// CANAL 1
+			else if (memCache[index][15:8] == tag && memCache[index][valid] && memCache[index][dirt]) begin
 					
-			end else begin
-					miss <= 1;
+					RAM[memCache[index][15:8]] = memCache[index][7:0]; // Leva o bloco da cache para a RAM
 					
-			
-			end
-			
-		
-			memCache[index][7:0] = data;
-			temp = data;
-			
-		end
-		if (!mode) begin // Leitura
-		
-			temp = memCache[index][7:0];
-		
-		end
-		
+					memCache[index][lru] = 1;			// Altera o LRU
+					memCache[index_plus][lru] = 0;	// LRU da segunda via
+				
+					memCache[index][7:0] = data;		// Atualiza o dado
+					
 
+					miss = 0;
+					hit = 1;
+					w_back = 1;
+			end
+			
+			// GRAVACAO VAZIA
+			// CANAL 2
+			else if (!memCache[index_plus][valid] || (!memCache[index_plus][dirt] && !memCache[index_plus][lru])) begin
+					
+					memCache[index_plus][valid] = 1; 		// Marca a validade
+					memCache[index_plus][dirt] = 1;			// Marca que está sujo
+					
+					memCache[index_plus][lru] = 1;			// Altera o LRU
+					memCache[index][lru] = 0;	            // LRU da primeira via
+					
+					memCache[index_plus][15:8] = tag;		// Atualiza a TAG
+					memCache[index_plus][7:0] = data;		// Atualiza o dado
+					
+					miss = 1;
+					hit = 0;
+					w_back = 0;
+			end
+			// GRAVACAO SUJA -> Valid e Dirt
+			// CANAL 2
+			else if (memCache[index_plus][15:8] == tag && memCache[index_plus][valid] && memCache[index_plus][dirt] ) begin
+					
+					RAM[memCache[index_plus][15:8]] = memCache[index_plus][7:0]; // Leva o bloco da cache para a RAM
+					
+					memCache[index_plus][lru] = 1;			// Altera o LRU
+					memCache[index][lru] = 0;	           // LRU da primeira via
+				
+					memCache[index_plus][7:0] = data;		// Atualiza o dado
+					
+					
+					miss = 0;
+					hit = 1;
+					w_back = 1;
+			end
+			
+			
+			
+			
+			
+			
+		end
 end
 
-assign data_out = temp;
+
+//memory _channel(add_memory,clock,data,ctrl_memory, response);
+
+
+//assign data_out = response_L1;
 
 
 endmodule
--- source_code_III/ram.v
+++ source_code_III/ram.v
@@ -13,12 +13,26 @@ wire [7:0] cacheData;
 
 
 initial begin
-
-		instruction = 19'b1010000000011111111;
+					   //  MODE INDEX      TAG         DATA
+		instruction = {1'b1, 2'b11, 8'b00000000,8'b00000001}; // Escreve no canal 1
 		#50;
 		
-		instruction = 19'b1110000000000000001;
-		#100;
+					   //  MODE INDEX      TAG         DATA
+		instruction = {1'b1, 2'b11, 8'b00000000,8'b11111111}; // Provoca um write back
+		#150;
+		
+					   //  MODE INDEX      TAG         DATA
+		instruction = {1'b1, 2'b11, 8'b00000001,8'b00001010}; // Escreve  no cabal 2 
+		#250;
+		
+					   //  MODE INDEX      TAG         DATA
+		instruction = {1'b1, 2'b11, 8'b00000001,8'b10000001}; // Provoca um write back
+		#350;
+		
+					   //  MODE INDEX      TAG         DATA
+		instruction = {1'b1, 2'b00, 8'b0000010,8'b00001111}; // escreve no canal 1
+		#450;
+		
 
 end
 
@@ -27,7 +41,7 @@ end
 //   1          2           8             8
 //	 [18]     [17:16]     [15:08]       [07:00]
 
-cache   lsd(clock, instruction[17:16], instruction[15:8], instruction[7:0], instruction[18], cacheData);
+cache   cache_L1(clock, instruction[17:16], instruction[15:8], instruction[7:0], instruction[18], cacheData);
 
 
 endmodule
